88ed45008ea4c1c0268d9f0640336ca5e42956fb
Ron Kurr <kurr@kurron.org>
2012-12-02 02:56:06 -0500

    Got a simple Java-based controller up and running.

---
+++ src/main/java/org/kurron/adapter/inbound/rest/ping/PingAdapter.java
@@ -0,0 +1,25 @@
+package org.kurron.adapter.inbound.rest.ping;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Just to verify that the Spring MVC framework is properly wired up.
+ */
+@Controller
+@RequestMapping( value = "/ping" )
+public class PingAdapter {
+
+    public PingAdapter() {
+        System.err.println("PingAdapter constructed!");
+    }
+
+    @RequestMapping( method = RequestMethod.GET, produces = "test/plain" )
+    public ResponseEntity<String> currentType()
+    {
+        return new ResponseEntity<String>( Long.toHexString( System.currentTimeMillis() ) , HttpStatus.OK );
+    }
+}
